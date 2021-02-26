637ad7944fe64088a89214b2c30b70045e3aebea
PSouza-10 <paulosouza300272@gmail.com>
2021-02-26 10:03:49 -0300

    chore: added forced redirect to dev frontend

--- src/controller/account/index.ts
+++ src/controller/account/index.ts
@@ -7,10 +7,8 @@ const FRONTEND = process.env.NODE_ENV
 	: process.env.FRONTEND_DEV
 const authenticate: RouteController = async (req, res) => {
 	const token = await jwt.sign(req.user._id.toString(), SECRET)
+
 	return res.redirect(FRONTEND + `?token=${token}`)
-}
-const getInfo: RouteController = async (req, res) => {
-	return res.json(req.user)
 }
 
 const getInfo: RouteController = async (req, res) => {
